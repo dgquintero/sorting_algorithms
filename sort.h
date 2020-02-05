185f7c60a28e73603e98624c30c8d9eedf5b5133
dgquintero <dgquintero02@hotmail.com>
2020-02-05 00:33:58 +0000

    header fixed betty style

--- sort.h
+++ sort.h
@@ -16,9 +16,7 @@ typedef struct listint_s
 	struct listint_s *next;
 } listint_t;
 
-/**
- * - - - - - PROTOTYPES - - - - - -
- */
+/*PROTOTYPES*/
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
 
